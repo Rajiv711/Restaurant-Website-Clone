e8d55b0c4134daaeaa08cd46e7aed581fd886461
Rajiv Mohite <mohiterajiv104@gmail.com>
2024-03-26 19:50:20 +0530

    2nd Lecture

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
-// const React = require("react");
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "h1" }, "Namste React H1"),
-    React.createElement("h2", { id: "h2" }, "Namste react H2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "h1" }, "Namste React H1"),
-    React.createElement("h2", { id: "h2" }, "Namste react H2"),
-  ])
-]);
+const parent = React.createElement("div", { id: "parent" }, 
+  React.createElement("div", { id: "child1" }, 
+    React.createElement("h1", { id: "h1" }, "Namste React H1"))
+);
 
 root.render(parent);
 
+ 
